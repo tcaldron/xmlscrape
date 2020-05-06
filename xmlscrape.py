af7fa0dd5e10b8ed4144cb4554b3e1fce31b3be0
Taylor Caldron <59539376+tcaldron@users.noreply.github.com>
2020-05-07 01:04:13 +0200

    Update xmlscrape.py
    Put things inside of a main() function with a docstring. Also updated variable names to meet PEP and match updates to functions file.

--- xmlscrape.py
+++ xmlscrape.py
@@ -6,42 +6,52 @@ import glob
 from progress.bar import ShadyBar
 import xmls_functions 
 
-# First we parse the arguments and verify existence of sitemaps.
-namespace = xmls_functions.parse_arguments()
-numberOfSitemaps = xmls_functions.detect_sitemaps()
+def main():
+    """In our main function we essentially run through the functions
+    from xmls_functions in sequence, while providing some user
+    feedback on current status and any processing done after
+    extraction."""
 
-# Next we initialize the progress bar.
-bar = ShadyBar("Processing:", max=numberOfSitemaps, 
-suffix="%(index)d XML Sitemaps Processed") 
+    # First we parse the arguments and verify existence of sitemaps.
+    namespace = xmls_functions.parse_arguments()
+    numberOfSitemaps = xmls_functions.detect_sitemaps()
 
-# We set up an empty list to fill via our processSitemap function.
-cleanedURLs = [] 
+    # Next we initialize the progress bar.
+    bar = ShadyBar("Processing:", max=numberOfSitemaps, 
+    suffix="%(index)d XML Sitemaps Processed") 
 
-# This is the meat of the application. We open up each sitemap
-# in the local directory, scrape out the URLs, and append
-# them to our list. 
-for filename in glob.glob('*.xml'): 
-    cleanedURLs = cleanedURLs \
-    + xmls_functions.process_sitemap(filename, namespace)
-    bar.next() # Incrementing the progress bar
+    # We set up an empty list to fill via our processSitemap function.
+    cleaned_urls = [] 
 
-# Shut down the progress bar once that's over. 
-bar.finish() 
+    # This is the meat of the application. We open up each sitemap
+    # in the local directory, scrape out the URLs, and append
+    # them to our list. 
+    for filename in glob.glob('*.xml'): 
+        cleaned_urls = cleaned_urls \
+        + xmls_functions.process_sitemap(filename, namespace)
+        bar.next() # Incrementing the progress bar
 
-# Deduplicating list by converting to a set, if required. It's fast,
-# but doesn't maintain the order of the original list, which is 
-# part of why a sorting option was also necessary. 
-if namespace.deduplicate:
-    cleanedURLs = xmls_functions.deduplicate_URLs(cleanedURLs)
+    # Shut down the progress bar once that's over. 
+    bar.finish() 
 
-# Sorting, if required.
-if namespace.sort:
-    print("Sorting URLs in alphanumeric order.")
-    cleanedURLs = sorted(cleanedURLs)
+    # Deduplicating list by converting to a set, if required. It's fast,
+    # but doesn't maintain the order of the original list, which is 
+    # part of why a sorting option was also necessary. 
+    if namespace.deduplicate:
+        cleaned_urls = xmls_functions.deduplicate_urls(cleaned_urls)
 
-# Writing final URL list to file.
-xmls_functions.output_URLs(cleanedURLs, namespace.o)
+    # Sorting, if required.
+    if namespace.sort:
+        print("Sorting URLs in alphanumeric order.")
+        cleaned_urls = sorted(cleaned_urls)
 
-# Providing user feedback that process is complete.
-print(f"Final URL list contains {len(cleanedURLs)} entries, output to" +  
-    f" {namespace.o}.")
+    # Writing final URL list to file.
+    xmls_functions.output_urls(cleaned_urls, namespace.o)
+
+    # Providing user feedback that process is complete.
+    print(f"Final URL list contains {len(cleaned_urls)} entries, output to" +  
+        f" {namespace.o}.")
+
+if __name__ == '__main__':
+    main()
+    
